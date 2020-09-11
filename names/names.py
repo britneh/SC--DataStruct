9a789dec4a6efef17839a6bdaeb67304366d4a3a
britneh <brhacket@gmail.com>
2020-09-11 10:16:22 -0700

    final commit

--- names/names.py
+++ names/names.py
@@ -1,5 +1,40 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False 
+            else:
+                return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,16 +47,29 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+#list for comparison
+name2_bst = BSTNode(names_2[0])
+for i in range(len(names_2)):
+    if i is not None:
+        name2_bst.insert(names_2[i])
+#add duplicates to list
+for name in names_1:
+    if name2_bst.contains(name):
+        duplicates.append(name)
+
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
+print(len(duplicates))
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
